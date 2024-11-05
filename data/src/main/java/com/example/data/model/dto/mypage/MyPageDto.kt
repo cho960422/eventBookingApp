1a4809effc0505dd856b020f7d884457ad51c6b3
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-11-05 17:20:55 +0900

    update: 마이페이지 DTO 수정
    직렬화 이름과 변수명 분리, UserEvent 객체 이름 변경된 부분 적용

--- data/src/main/java/com/example/data/model/dto/mypage/MyPageDto.kt
+++ data/src/main/java/com/example/data/model/dto/mypage/MyPageDto.kt
@@ -1,9 +1,18 @@
 package com.example.data.model.dto.mypage
 
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MyPageDto(
+    @SerializedName("birth")
     val birth: String,
-    val bookmarkList: List<Bookmark>,
+    @SerializedName("bookmarkList")
+    val bookmarkList: List<UserEventDto>,
+    @SerializedName("character")
     val character: String,
+    @SerializedName("name")
     val name: String,
-    val userEventList: List<UserEvent>
+    @SerializedName("userEventList")
+    val userEventList: List<UserEventDto>
 )
