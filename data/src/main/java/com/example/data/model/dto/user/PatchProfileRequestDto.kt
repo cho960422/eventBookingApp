efbb34cab8ac45046f6250a185ba7d0e99271d66
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-11-05 17:23:02 +0900

    feat: PatchProfileRequestDto 정의
    프로필 수정에 사용될 요청 객체 정의, 외부로 나가는 DTO

---
+++ data/src/main/java/com/example/data/model/dto/user/PatchProfileRequestDto.kt
@@ -0,0 +1,18 @@
+package com.example.data.model.dto.user
+
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PatchProfileRequestDto(
+    @SerializedName("username")
+    val username: String,
+    @SerializedName("introduction")
+    val introduction: String,
+    @SerializedName("local_name")
+    val localName: String,
+    @SerializedName("birth")
+    val birth: String,
+    @SerializedName("character")
+    val character: String
+)
