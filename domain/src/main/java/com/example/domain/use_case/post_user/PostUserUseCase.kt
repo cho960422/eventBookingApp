460c10b43e97593f7d62fba9a041b3a68acde0ce
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-11-05 17:25:49 +0900

    update(PostUserUseCase.kt): 응답 양식 변경
    usecase 응답 양식 flow로 변경, 내부 로직 임시 작성

--- domain/src/main/java/com/example/domain/use_case/post_user/PostUserUseCase.kt
+++ domain/src/main/java/com/example/domain/use_case/post_user/PostUserUseCase.kt
@@ -1,17 +1,23 @@
 package com.example.domain.use_case.post_user
 
 import com.example.domain.entities.state.Response
-import com.example.domain.model.User
+import com.example.domain.entities.user.User
 import com.example.domain.repository.UserRepository
 import com.example.domain.use_case.BaseUseCase
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class PostUserUseCase @Inject constructor(private val repository: UserRepository) :
     BaseUseCase<User>() {
-    operator fun invoke(request: User): Flow<Response<User>> = execute {
-        repository.postUser(
-            request = request
+    operator fun invoke(request: User): Flow<Response<Boolean>> = flow {
+
+        emit(
+            Response.Success(
+                repository.signUpUser(
+                    request = request
+                )
+            )
         )
     }
 }
