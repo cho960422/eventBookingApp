2d07675061ef27c9efe56de210589c1443cd93a5
cho960422 <87304487+cho960422@users.noreply.github.com>
2024-11-05 17:26:52 +0900

    feat(UserRepository.kt): 마이페이지 인터페이스 정의
    마이페이지 데이터 요청 및 응답 인터페이스 정의

--- domain/src/main/java/com/example/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/example/domain/repository/UserRepository.kt
@@ -1,12 +1,14 @@
 package com.example.domain.repository
 
-import com.example.domain.model.EventUserListEntity
-import com.example.domain.model.PatchUserRequestEntity
-import com.example.domain.model.User
+import com.example.domain.entities.mypage.MyPageEntity
+import com.example.domain.entities.event.EventUserListEntity
+import com.example.domain.entities.user.PatchUserRequestEntity
+import com.example.domain.entities.user.User
 
 interface UserRepository {
     suspend fun signUpUser(request: User): Boolean
     suspend fun getUser(): User
+    suspend fun getMyPageInformation(): MyPageEntity
     suspend fun patchUser(request: PatchUserRequestEntity): Boolean
     suspend fun getEventJoinUsers(eventId: Long): List<EventUserListEntity>
 }
